2b8f42199d9e44961b72373cabd1cc8452180317
Julio S <85770548+Julius-Sol@users.noreply.github.com>
2023-08-17 18:20:53 -0400

    Finalize mobile version

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
 let menuOpen = false;
 
 
@@ -6,9 +7,13 @@ let menuOpen = false;
 menuBtn.addEventListener('click',() => {
   if(!menuOpen){
     menuBtn.classList.add('open');
+    nav.classList.remove('hidden')
+    document.body.style.overflow = "hidden";
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    nav.classList.add('hidden')
+    document.body.style.overflow = "auto";
     menuOpen = false;
   }
 });
